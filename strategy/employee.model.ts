3e76814ca51e1db1ae914e09f4b760d71d5751b2
Edy Segura <edysegura@gmail.com>
2019-12-30 15:10:56 -0300

    Add employee model

---
+++ strategy/employee.model.ts
@@ -0,0 +1,21 @@
+export interface Employee {
+  getSalary(): number;
+}
+
+export class Contractor implements Employee {
+  getSalary(): number {
+    return 2000;
+  }
+}
+
+export class FulltimeEmployeer implements Employee {
+  getSalary(): number {
+    return 1000;
+  }
+}
+
+export class Manager implements Employee {
+  getSalary(): number {
+    return 5000;
+  }
+}
