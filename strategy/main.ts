82bed4c5c9894c5c2a78ecdf422da82e7ce53d3f
Edy Segura <edysegura@gmail.com>
2024-05-14 14:43:45 -0300

    chore: update strategy example

--- strategy/main.ts
+++ strategy/main.ts
@@ -1,9 +1,20 @@
-import { SalaryService } from './calculator.service';
-import { Contractor, FulltimeEmployee, Manager } from './employee.model';
+import { SalaryService } from './calculator.service'
+import { Contractor, FulltimeEmployee, Manager } from './employee.model'
 
-const salaryService = new SalaryService();
-salaryService.calculate(new FulltimeEmployee());
-salaryService.calculate(new Contractor());
-salaryService.calculate(new Manager());
+const salaryService = new SalaryService()
+salaryService.calculate(new FulltimeEmployee())
+salaryService.calculate(new Contractor())
+salaryService.calculate(new Manager())
 
-console.log('Total: ', salaryService.getTotal());
+const formatter =
+  Intl.NumberFormat &&
+  new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+const totalPayroll =
+  (formatter && formatter.format(salaryService.getTotal())) ||
+  salaryService.getTotal()
+
+console.log('Total payroll is: ', totalPayroll)
