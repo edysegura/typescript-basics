656410052e5b21fe399f85e23aee89fb09460318
Edy Segura <edysegura@gmail.com>
2025-08-11 22:09:14 -0300

    fix: typo

--- adapter/app.ts
+++ adapter/app.ts
@@ -7,7 +7,7 @@ const postUrl = 'https://jsonplaceholder.typicode.com/posts'
 const payload = { title: 'foo', body: 'bar', userId: 1 }
 
 async function makeRequests(httpClient: HttpRequest) {
-  console.log(`HTTP Client Implementation: ${httpClient}`)
+  console.log(`HTTP Client Implementation: ${httpClient.clientName}`)
   const getResponse = await httpClient.get(url)
   console.log('GET response: ✅')
   const postResponse = await httpClient.post(postUrl, payload)
